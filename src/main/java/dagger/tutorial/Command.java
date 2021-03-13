b64dd1a000b5a575496da74ee6aa94e719f3b815
Guowei Lv <lvguowei1002@gmail.com>
2021-03-13 14:38:47 +0200

    Command.java

--- src/main/java/dagger/tutorial/Command.java
+++ src/main/java/dagger/tutorial/Command.java
@@ -1,11 +1,49 @@
 package dagger.tutorial;
 
 import java.util.List;
+import java.util.Optional;
+
+import dagger.tutorial.CommandRouterFactory;
 
 interface Command {
-  Status handleInput(List<String> input);
-  enum Status {
-    INVALID,
-    HANDLED
-  }
+
+    Result handleInput(List<String> input);
+
+    class Result {
+        private final Status status;
+        private final Optional<CommandRouter> nestedCommandRouter;
+
+        private Result(Status status, Optional<CommandRouter> nestedCommandRouter) {
+            this.status = status;
+            this.nestedCommandRouter = nestedCommandRouter;
+        }
+
+        static Result invalid() {
+            return new Result(Status.INVALID, Optional.empty());
+        }
+
+        static Result handled() {
+            return new Result(Status.HANDLED, Optional.empty());
+        }
+
+        static Result inputCompleted() {
+            return new Result(Status.INPUT_COMPLETED, Optional.empty());
+        }
+
+        static Result enterNestedCommandSet(CommandRouter nestedCommandRouter) {
+            return new Result(Status.HANDLED, Optional.of(nestedCommandRouter));
+        }
+
+        Status status() {
+            return status;
+        }
+
+        Optional<CommandRouter> nestedCommandRouter() {
+            return nestedCommandRouter;
+        }
+    }
+
+    enum Status {
+        INVALID, HANDLED, INPUT_COMPLETED
+    }
 }
